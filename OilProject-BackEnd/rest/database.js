3d9114cd5041b816dddfe8079da54585db57e746
Jaogodela <166837573+Jaogodela@users.noreply.github.com>
2024-12-14 15:37:11 -0300

    update

--- OilProject-BackEnd/rest/database.js
+++ OilProject-BackEnd/rest/database.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: 'localhost',
-  port: '2000',
+  port: '3306',
   user: 'root',
   password: '1234',
   database: 'oilproject'
