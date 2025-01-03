a7cf8e23414427f09692e8725281d47b878b6f06
Jaogodela <166837573+Jaogodela@users.noreply.github.com>
2025-01-03 13:56:52 +0000

    Sei nao

--- OilProject-BackEnd/rest/routes/deleteProduct.js
+++ OilProject-BackEnd/rest/routes/deleteProduct.js
@@ -2,7 +2,6 @@ module.exports = function (rest) {
     rest.post('/api/post/products/deleteProduct', (req, res) => {
         const content = req.body;
         console.log(content);
-        console.log(content.length);
         res.status(200).send()
 
     })
--- OilProject-BackEnd/rest/routes/registerProduct.js
+++ OilProject-BackEnd/rest/routes/registerProduct.js
@@ -1,5 +1,10 @@
+const cors = require('cors'); 
+// encher galão (nao esquecer)
+
 module.exports = function (rest) {
     rest.post('/api/post/products/registerProduct', async (req, res) => {
+        const response = req.body;
+        console.log(response);
         res.status(200).send('a');
     })
 }
